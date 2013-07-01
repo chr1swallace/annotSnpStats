cd6faa0319c5d3d42a84453c948d666531a7ed1b
chr1swallace <chris.wallace@cimr.cam.ac.uk>
2013-07-01 09:03:18 +0100

    updated docs for snp.match, sample.match

--- R/snp-match.R
+++ R/snp-match.R
@@ -16,13 +16,20 @@ sample.id <- function(x,col.x) {
 ##' Match snps in two aSnpMatrix objects
 ##'
 ##' Extract indices of overlapping SNPs in two aSnpMatrix objects.  By
-##' default, match is on rownames(x@snps), rownames(y@snps).
+##' default, match is on \code{rownames(x@@snps)}, \code{rownames(y@@snps)}.
 ##' @title snp.match
 ##' @param x aSnpMatrix object
 ##' @param y aSnpMatrix object
-##' @param col.x optional, column identifier for x@snps for matching
-##' @param col.y optional, column identifier for y@snps for matching
-##' @return 
+##' @param col.x optional, column identifier for \code{x@@snps} for matching
+##' @param col.y optional, column identifier for \code{y@@snps} for matching
+##' @return a named list of length two with elements
+##' \describe{
+##'
+##' \item{x}{indices of matching SNPs in x}
+##'
+##' \item{y}{indices of matching SNPs in y}
+##'
+##' }
 ##' @author Chris Wallace
 snp.match <- function(x,y,col.x=0,col.y=0) {
   id.x <- snp.id(x,col.x)
@@ -35,16 +42,23 @@ snp.match <- function(x,y,col.x=0,col.y=0) {
   return(list(x=m.x, y=m.y))
 }
 
-##' Match snps in two aSnpMatrix objects
+##' Match samples in two aSnpMatrix objects
 ##'
-##' Extract indices of overlapping SNPs in two aSnpMatrix objects.  By
-##' default, match is on rownames(x@snps), rownames(y@snps).
+##' Extract indices of overlapping samples in two aSnpMatrix objects.  By
+##' default, match is on \code{rownames(x@@samples)}, \code{rownames(y@@snps)}.
 ##' @title snp.match
 ##' @param x aSnpMatrix object
 ##' @param y aSnpMatrix object
-##' @param col.x optional, column identifier for x@snps for matching
-##' @param col.y optional, column identifier for y@snps for matching
-##' @return 
+##' @param col.x optional, column identifier for \code{x@@samples} for matching
+##' @param col.y optional, column identifier for \code{y@@samples} for matching
+##' @return  a named list of length two with elements
+##' \describe{
+##'
+##' \item{x}{indices of matching samples in x}
+##'
+##' \item{y}{indices of matching samples in y}
+##'
+##' }
 ##' @author Chris Wallace
 sample.match <- function(x,y,col.x=0,col.y=0) {
   id.x <- sample.id(x,col.x)
