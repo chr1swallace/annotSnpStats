fe298f15184e70570927944b3ea0285891b397ab
chr1swallace <chris.wallace@cimr.cam.ac.uk>
2013-08-09 12:38:51 +0100

    added annot.plink

---
+++ R/conversion.R
@@ -0,0 +1,13 @@
+##' Convert output of \code{read.plink()} to an \code{aSnpMatrix} object
+##'
+##' @title annot.plink
+##' @param obj output of \code{read.plink()}
+##' @return aSnpMatrix object
+##' @author Chris Wallace
+##' @export
+annot.plink <- function(obj) {
+  new("aSnpMatrix",
+      .Data=obj@genotypes,
+      snps=obj@fam,
+      samples=obj@fam)
+}
