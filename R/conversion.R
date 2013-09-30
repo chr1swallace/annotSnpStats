837c43b313b2b996f6fabca8785aa9d4f3faadb8
chr1swallace <chris.wallace@cimr.cam.ac.uk>
2013-09-30 12:26:10 +0100

    added annot.read.plink, a wrapper for snpStats::read.plink() followed by annot.plink()

--- R/conversion.R
+++ R/conversion.R
@@ -9,5 +9,34 @@ annot.plink <- function(obj) {
   new("aSnpMatrix",
       .Data=obj$genotypes,
       snps=obj$map,
-      samples=obj$fam)
+      samples=obj$fam,
+      alleles=c("allele.1","allele.2"),
+      phenotype="affected")
+}
+
+##' Read plink input files and convert to an aSnpMatrix object
+##'
+##' @title annot.read.plink
+##' @param filestub character object specifying the stub of a plink
+##' file.  Files with .bed .bim, and .fam extensions will be read
+##' using snpStats::read.plink and the result converted to an
+##' aSnpMatrix object
+##' @return aSnpMatrix object
+##' @author Chris Wallace
+##' @export
+annot.read.plink <- function(filestub) {
+  obj <- read.plink(fam=sprintf("%s.fam",filestub),
+                       bed=sprintf("%s.bed",filestub),
+                       bim=sprintf("%s.bim",filestub))
+  new("aSnpMatrix",
+      .Data=obj$genotypes,
+      snps=obj$map,
+      samples=obj$fam,
+      alleles=c("allele.1","allele.2"),
+      phenotype="affected")
+}
+
+setAs("aSnpMatrix", "SnpMatrix", function(from) new("SnpMatrix", from@.Data))
+sm <- function(obj) {
+  as(obj, "SnpMatrix")
 }
